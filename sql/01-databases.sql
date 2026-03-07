ae80a0df7029a190b627f7ebed38b344f38cf526
michaelfang <yingchih.fang@gmail.com>
2026-03-07 13:16:31 +0800

    fix .env

--- sql/01-databases.sql
+++ sql/01-databases.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `asterisk`;
 CREATE DATABASE IF NOT EXISTS `asteriskcdrdb`;
 
 -- create root user and grant rights
-GRANT ALL ON asterisk.* TO 'asterisk'@'127.0.0.1' IDENTIFIED BY 'asterisk';
-GRANT ALL ON asterisk.* TO 'asterisk'@'%' IDENTIFIED BY 'asterisk';
-GRANT ALL ON asteriskcdrdb.* TO 'asterisk'@'127.0.0.1' IDENTIFIED BY 'asterisk';
-GRANT ALL ON asteriskcdrdb.* TO 'asterisk'@'%' IDENTIFIED BY 'asterisk';
+GRANT ALL ON asterisk.* TO 'asterisk'@'127.0.0.1' IDENTIFIED BY 'your_asterisk_password';
+GRANT ALL ON asterisk.* TO 'asterisk'@'%' IDENTIFIED BY 'your_asterisk_password';
+GRANT ALL ON asteriskcdrdb.* TO 'asterisk'@'127.0.0.1' IDENTIFIED BY 'your_asterisk_password';
+GRANT ALL ON asteriskcdrdb.* TO 'asterisk'@'%' IDENTIFIED BY 'your_asterisk_password';
